f3abe9ed7e18bf08a932e579a7560add6bd070e7
Deyvison Samuel Gomes do Nascimento <deyvisonnascimento2025@gmail.com>
2026-09-01 15:48:30 -0300

    feat: adiciona o texto de Lore da Fase 1

--- js/plugins/BancoQuestoes.js
+++ js/plugins/BancoQuestoes.js
@@ -17,42 +17,42 @@ const BancoQuestoes = {
     1: [
 
         {
-            question: "Quanto é 7 + 5?",
+            question: "Para fazer um bolo, Júlia usou 1/4 de xícara de açúcar pela manhã e 2/4 de xícara à tarde. Qual a fração total de açúcar que ela usou?",
 
             answers: [
-                "10",
-                "11",
-                "12",
-                "13"
+                "3/8 de xícara",
+                "3/4 de xícara",
+                "2/4 de xícara",
+                "1/4 de xícara"
+            ],
+
+            correct: 1
+        },
+
+        {
+            question: "Lucas tinha 5/6 de uma barra de chocolate e comeu 1/2 dessa barra. Quanto sobrou do chocolate?",
+
+            answers: [
+                "4/4 de barra",
+                "2/6 de barra",
+                "1/3 de barra",
+                "4/6 de barra"
             ],
 
             correct: 2
         },
 
         {
-            question: "Quanto é 8 + 6?",
+            question: "Um fazendeiro usou 1/3 do seu terreno para plantar milho e 2/5 para plantar feijão. Que fração do terreno já foi utilizada para o cultivo?",
 
             answers: [
-                "12",
-                "13",
-                "14",
-                "15"
+                "3/8 do terreno",
+                "11/15 do terreno",
+                "3/15 do terreno",
+                "1/15 do terreno"
             ],
 
-            correct: 2
-        },
-
-        {
-            question: "Quanto é 15 - 7?",
-
-            answers: [
-                "6",
-                "7",
-                "8",
-                "9"
-            ],
-
-            correct: 2
+            correct: 1
         }
 
     ],
